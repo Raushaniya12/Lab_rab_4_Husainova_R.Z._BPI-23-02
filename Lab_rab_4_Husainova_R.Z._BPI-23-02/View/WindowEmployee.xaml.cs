ab671ec9379d42dfa4e2e7ef7a710d49a38d01a7
Raushaniya12 <rausaniahusainova724@gmail.com>
2025-10-24 11:36:54 +0500

    .

--- Lab_rab_4_Husainova_R.Z._BPI-23-02/View/WindowEmployee.xaml.cs
+++ Lab_rab_4_Husainova_R.Z._BPI-23-02/View/WindowEmployee.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Windows;
-using Lab_rab_4_Husainova_R.Z._BPI_23_02.Helper;
+﻿using Lab_rab_4_Husainova_R.Z._BPI_23_02.Helper;
 using Lab_rab_4_Husainova_R.Z._BPI_23_02.Model;
 using Lab_rab_4_Husainova_R.Z._BPI_23_02.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
 
 namespace Lab_rab_4_Husainova_R.Z._BPI_23_02.View
 {
@@ -13,6 +15,32 @@ namespace Lab_rab_4_Husainova_R.Z._BPI_23_02.View
         {
             InitializeComponent();
             DataContext = PersonViewModel.Instance;
+        }
+    
+    private void SwitchTheme(string themeName)
+        {
+            var uri = new Uri($"Themes/{themeName}.xaml", UriKind.Relative);
+            var theme = new ResourceDictionary { Source = uri };
+
+            var oldTheme = Application.Current.Resources.MergedDictionaries
+                .FirstOrDefault(d => d.Source?.OriginalString.Contains("Theme") == true);
+            if (oldTheme != null)
+                Application.Current.Resources.MergedDictionaries.Remove(oldTheme);
+
+            Application.Current.Resources.MergedDictionaries.Add(theme);
+        }
+        private void BtnLight_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchTheme("LightTheme");
+            BtnLight.Tag = "Active";
+            BtnDark.Tag = null;
+        }
+
+        private void BtnDark_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchTheme("DarkTheme");
+            BtnDark.Tag = "Active";
+            BtnLight.Tag = null;
         }
     }
 }
