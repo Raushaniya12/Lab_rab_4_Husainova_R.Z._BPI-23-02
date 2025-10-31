2ddf96b2ad05316ce630acd0ac2ac423de20b1c2
Raushaniya12 <rausaniahusainova724@gmail.com>
2025-10-31 08:51:47 +0500

    .

--- Lab_rab_4_Husainova_R.Z._BPI-23-02/ViewModel/PersonViewModel.cs
+++ Lab_rab_4_Husainova_R.Z._BPI-23-02/ViewModel/PersonViewModel.cs
@@ -2,18 +2,69 @@
 using Lab_rab_4_Husainova_R.Z._BPI_23_02.Model;
 using Lab_rab_4_Husainova_R.Z._BPI_23_02.View;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Lab_rab_4_Husainova_R.Z._BPI_23_02.ViewModel
 {
+    public class PersonEditContext : INotifyPropertyChanged
+    {
+        public ObservableCollection<Role> Roles => RoleViewModel.Instance.ListRole;
+        public PersonDpo Person { get; }
+        public ICommand SaveCommand { get; }
+
+        public PersonEditContext(PersonDpo person, Action saveAction)
+        {
+            Person = person;
+            SaveCommand = new RelayCommand(
+                _ => saveAction(),
+                _ => !string.IsNullOrWhiteSpace(person.FirstName?.Trim()) &&
+                     !string.IsNullOrWhiteSpace(person.LastName?.Trim()) &&
+                     !string.IsNullOrWhiteSpace(person.RoleName?.Trim())
+            );
+        }
+
+        public int Id
+        {
+            get => Person.Id;
+            set => Person.Id = value;
+        }
+
+        public string RoleName
+        {
+            get => Person.RoleName;
+            set => Person.RoleName = value;
+        }
+
+        public string FirstName
+        {
+            get => Person.FirstName;
+            set => Person.FirstName = value;
+        }
+
+        public string LastName
+        {
+            get => Person.LastName;
+            set => Person.LastName = value;
+        }
+
+        public DateTime Birthday
+        {
+            get => Person.Birthday;
+            set => Person.Birthday = value;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
     public class PersonViewModel : INotifyPropertyChanged
     {
         private static PersonViewModel _instance;
@@ -28,206 +79,135 @@ namespace Lab_rab_4_Husainova_R.Z._BPI_23_02.ViewModel
                 return _instance;
             }
         }
+
         private PersonDpo selectedPersonDpo;
         public PersonDpo SelectedPersonDpo
         {
-            get { return selectedPersonDpo; }
+            get => selectedPersonDpo;
             set
             {
-                selectedPersonDpo = value; 
-                OnPropertyChanged("SelectedPersonDpo");
-                AddPerson.CanExecute(true);
-                EditPerson.CanExecute(true);
-                DeletePerson.CanExecute(true);
+                selectedPersonDpo = value;
+                OnPropertyChanged();
             }
         }
-        public ObservableCollection<Person> ListPerson { get; set; } = 
-            new ObservableCollection<Person>();
-        public ObservableCollection<PersonDpo> ListPersonDpo { get; set; } = 
-            new ObservableCollection<PersonDpo>();
+
+        public ObservableCollection<Person> ListPerson { get; set; } = new ObservableCollection<Person>();
+        public ObservableCollection<PersonDpo> ListPersonDpo { get; set; } = new ObservableCollection<PersonDpo>();
+
         public PersonViewModel()
         {
-            this.ListPerson.Add(new Person { Id = 1, RoleId = 1, FirstName = "Иван",
-                LastName = "Иванов", Birthday = new DateTime(1980, 2, 28) });
-            this.ListPerson.Add(new Person{Id = 2, RoleId = 2, FirstName = "Петр",
-                LastName = "Петров", Birthday = new DateTime(1981, 3, 20) });
-            this.ListPerson.Add(new Person { Id = 3, RoleId = 3, FirstName = "Виктор",
-                LastName = "Викторов", Birthday = new DateTime(1982, 4, 15) });
-            this.ListPerson.Add(new Person {Id = 4, RoleId = 3, FirstName = "Сидор",
-                LastName = "Сидоров", Birthday = new DateTime(1983, 5, 10) });
-            ListPersonDpo = GetListPersonDpo();
+            ListPerson.Add(new Person { Id = 1, RoleId = 1, FirstName = "Иван", LastName = "Иванов", Birthday = new DateTime(1980, 2, 28) });
+            ListPerson.Add(new Person { Id = 2, RoleId = 2, FirstName = "Петр", LastName = "Петров", Birthday = new DateTime(1981, 3, 20) });
+            ListPerson.Add(new Person { Id = 3, RoleId = 3, FirstName = "Виктор", LastName = "Викторов", Birthday = new DateTime(1982, 4, 15) });
+            ListPerson.Add(new Person { Id = 4, RoleId = 3, FirstName = "Сидор", LastName = "Сидоров", Birthday = new DateTime(1983, 5, 10) });
+            RefreshListPersonDpo();
         }
-        public ObservableCollection<PersonDpo> GetListPersonDpo()
+
+        private void RefreshListPersonDpo()
         {
+            ListPersonDpo.Clear();
             foreach (var person in ListPerson)
             {
-                PersonDpo p = new PersonDpo(); 
-                p = p.CopyFromPerson(person); 
-                ListPersonDpo.Add(p);
+                ListPersonDpo.Add(new PersonDpo().CopyFromPerson(person));
             }
-            return ListPersonDpo;
         }
-        public int MaxId()
-        {
-            int max = 0;
-            foreach (var r in this.ListPerson)
-            {
-                if (max < r.Id)
-                {
-                    max = r.Id;
-                };
-            }
-            return max;
-        }
+
+        public int MaxId() => ListPerson.Count == 0 ? 0 : ListPerson.Max(p => p.Id);
+
         private RelayCommand addPerson;
-        public RelayCommand AddPerson
+        public RelayCommand AddPerson => addPerson = new RelayCommand(_ =>
         {
-            get
+            var wnPerson = new WindowNewEmployee { Title = "Новый сотрудник" };
+            var personDpo = new PersonDpo
             {
-                return addPerson ??
-                (addPerson = new RelayCommand(obj =>
-                {
-                    WindowNewEmployee wnPerson = new WindowNewEmployee
-                    {
-                        Title = "Новый сотрудник"
-                    };
-                    int maxIdPerson = MaxId() + 1;
-                    PersonDpo per = new PersonDpo
-                    {
-                        Id = maxIdPerson, 
-                        Birthday = DateTime.Now
-                    };
-                    wnPerson.DataContext = per;
-                    if (wnPerson.ShowDialog() == true)
-                    {
-                        Role r = (Role)wnPerson.CbRole.SelectedValue;
-                        per.RoleName = r.NameRole; 
-                        ListPersonDpo.Add(per);
-                        Person p = new Person();
-                        p = p.CopyFromPersonDPO(per); 
-                        ListPerson.Add(p);
-                    }
-                },
-                (obj) => true));
+                Id = MaxId() + 1,
+                Birthday = DateTime.Now
+            };
+
+            var context = new PersonEditContext(personDpo, () =>
+            {
+                wnPerson.DialogResult = true;
+                wnPerson.Close();
+            });
+
+            wnPerson.DataContext = context;
+
+            if (wnPerson.ShowDialog() == true)
+            {
+                ListPersonDpo.Add(personDpo);
+                ListPerson.Add(new Person().CopyFromPersonDPO(personDpo));
             }
-        }
-        private RelayCommand editPerson; 
-        public RelayCommand EditPerson
+        });
+
+        private RelayCommand editPerson;
+        public RelayCommand EditPerson => editPerson = new RelayCommand(_ =>
         {
-            get
+            var wnPerson = new WindowNewEmployee { Title = "Редактирование данных сотрудника" };
+            var tempPerson = SelectedPersonDpo.ShallowCopy();
+
+            var context = new PersonEditContext(tempPerson, () =>
             {
-                return editPerson ??
-                (editPerson = new RelayCommand(obj =>
+                wnPerson.DialogResult = true;
+                wnPerson.Close();
+            });
+
+            wnPerson.DataContext = context;
+
+            if (wnPerson.ShowDialog() == true)
+            {
+                SelectedPersonDpo.RoleName = tempPerson.RoleName;
+                SelectedPersonDpo.FirstName = tempPerson.FirstName;
+                SelectedPersonDpo.LastName = tempPerson.LastName;
+                SelectedPersonDpo.Birthday = tempPerson.Birthday;
+
+                var existingPerson = ListPerson.FirstOrDefault(p => p.Id == SelectedPersonDpo.Id);
+                if (existingPerson != null)
                 {
-                    WindowNewEmployee wnPerson = new WindowNewEmployee()
-                    {
-                        Title = "Редактирование данных сотрудника",
-                    };
-                    PersonDpo personDpo = SelectedPersonDpo;
-                    PersonDpo tempPerson = new PersonDpo();
-                    tempPerson = personDpo.ShallowCopy();
-                    wnPerson.DataContext = tempPerson;
-                    if (wnPerson.ShowDialog() == true)
-                    {
-                        Role r = (Role) wnPerson.CbRole.SelectedValue;
-                        personDpo.RoleName = r.NameRole;
-                        personDpo.FirstName = tempPerson.FirstName;
-                        personDpo.LastName = tempPerson.LastName;
-                        personDpo.Birthday = tempPerson.Birthday;
-                        FindPerson finder = new FindPerson(personDpo.Id);
-                        List<Person> listPerson = ListPerson.ToList(); 
-                        Person p = listPerson.Find(new Predicate<Person>
-                            (finder.PersonPredicate));
-                        p = p.CopyFromPersonDPO(personDpo);
-                    }
-                }, (obj) => SelectedPersonDpo != null && ListPersonDpo.Count > 0));
+                    existingPerson.CopyFromPersonDPO(SelectedPersonDpo);
+                }
             }
-        }
+        }, _ => SelectedPersonDpo != null);
+
         private RelayCommand deletePerson;
-        public RelayCommand DeletePerson
+        public RelayCommand DeletePerson => deletePerson = new RelayCommand(_ =>
         {
-            get
+            var dpo = SelectedPersonDpo;
+            if (dpo == null) return;
+
+            var result = MessageBox.Show($"Удалить сотрудника:\n{dpo.LastName} {dpo.FirstName}",
+                "Подтверждение", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.OK)
             {
-                return deletePerson ??
-                (deletePerson = new RelayCommand(obj =>
-                {
-                    PersonDpo person = SelectedPersonDpo;
-                    MessageBoxResult result = MessageBox.Show("Удалить данные по сотруднику: \n"
-                        + person.LastName + " " + person.FirstName,
-                        "Предупреждение", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
-                    if (result == MessageBoxResult.OK)
-                    {
-                        ListPersonDpo.Remove(person);
-                        Person per = new Person();
-                        per = per.CopyFromPersonDPO(person);
-                        ListPerson.Remove(per);
-                    }
-                }, (obj) => SelectedPersonDpo != null && ListPersonDpo.Count > 0));
+                ListPersonDpo.Remove(dpo);
+                var person = ListPerson.FirstOrDefault(p => p.Id == dpo.Id);
+                if (person != null)
+                    ListPerson.Remove(person);
+
+                SelectedPersonDpo = null;
             }
-        }
-        
+        }, _ => SelectedPersonDpo != null);
+
         private RelayCommand openEmployeeWindow;
-        public RelayCommand OpenEmployeeWindow
+        public RelayCommand OpenEmployeeWindow => openEmployeeWindow = new RelayCommand(_ =>
         {
-            get
-            {
-                if (openEmployeeWindow == null)
-                {
-                    openEmployeeWindow = new RelayCommand(_ =>
-                    {
-                        new WindowEmployee { DataContext = PersonViewModel.Instance }.Show();
-                    });
-                }
-                return openEmployeeWindow;
-            }
-        }
+            new WindowEmployee { DataContext = PersonViewModel.Instance }.Show();
+        });
+
         private RelayCommand openRoleWindow;
-        public RelayCommand OpenRoleWindow
+        public RelayCommand OpenRoleWindow => openRoleWindow = new RelayCommand(_ =>
         {
-            get
-            {
-                if (openRoleWindow == null)
-                {
-                    openRoleWindow = new RelayCommand(_ =>
-                    {
-                        new WindowRole { DataContext = RoleViewModel.Instance }.Show();
-                    });
-                }
-                return openRoleWindow;
-            }
-        }
+            new WindowRole { DataContext = RoleViewModel.Instance }.Show();
+        });
+
         private static RelayCommand switchLightTheme;
-        public static RelayCommand SwitchLightTheme
-        {
-            get
-            {
-                if (switchLightTheme == null)
-                {
-                    switchLightTheme = new RelayCommand(_ => ThemeManager.ApplyTheme("LightTheme"));
-                }
-                return switchLightTheme;
-            }
-        }
+        public static RelayCommand SwitchLightTheme => switchLightTheme = new RelayCommand(_ => ThemeManager.ApplyTheme("LightTheme"));
 
         private static RelayCommand switchDarkTheme;
-        public static RelayCommand SwitchDarkTheme
-        {
-            get
-            {
-                if (switchDarkTheme == null)
-                {
-                    switchDarkTheme = new RelayCommand(_ => ThemeManager.ApplyTheme("DarkTheme"));
-                }
-                return switchDarkTheme;
-            }
-        }
+        public static RelayCommand SwitchDarkTheme => switchDarkTheme = new RelayCommand(_ => ThemeManager.ApplyTheme("DarkTheme"));
 
-
-        public event PropertyChangedEventHandler PropertyChanged; 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
     }
 }
